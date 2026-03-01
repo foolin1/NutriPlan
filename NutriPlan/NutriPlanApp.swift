e067a3b7e42d9cf61618b4771022f34d59cd32cd
r1nzu <roofer1809@gmail.com>
2026-03-01 14:30:28 +0300

    Initial Commit

---
+++ NutriPlan/NutriPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriPlanApp.swift
+//  NutriPlan
+//
+//  Created by R1nzu on 1.03.26.
+//
+
+import SwiftUI
+
+@main
+struct NutriPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
